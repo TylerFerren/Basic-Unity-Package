04dd9c604401f1b895487a0b8cd941ad364a9d0d
TylerFerren <77201948+TylerFerren@users.noreply.github.com>
2023-02-18 20:47:52 -0500

    Update CollectionManagerEditor.cs

--- Editor/Collections and Inventories/CollectionManagerEditor.cs
+++ Editor/Collections and Inventories/CollectionManagerEditor.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Codesign.Collections;
-using Sirenix;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
