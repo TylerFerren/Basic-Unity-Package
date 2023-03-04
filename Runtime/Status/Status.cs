bb81cfa7ca7d129b142e8a3df143ea05d954def8
TylerFerren <77201948+TylerFerren@users.noreply.github.com>
2023-03-03 23:46:46 -0500

    Update Status.cs

--- Runtime/Status/Status.cs
+++ Runtime/Status/Status.cs
@@ -35,7 +35,7 @@ namespace Codesign
             if (currentValue == 0) currentValue = maxValue.Value;
             if (automaticRefill && currentValue < maxValue.Value) ActiveAdjustment = StartCoroutine(AdjustOverTime(refillRate, refillDelay));
             StatusUpdate.Invoke(this);
-            maxValue.OnValueUpdate.AddListener(delegate{ AdjustStatus(0);});
+            maxValue.OnValueUpdate.AddListener(delegate{ AdjustStatus(maxValue-currentValue);});
         }
 
         public void OnDisable()
