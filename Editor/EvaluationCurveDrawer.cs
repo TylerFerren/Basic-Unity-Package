9ee918726829abd9649216d086ea589d5768c643
TylerFerren <77201948+TylerFerren@users.noreply.github.com>
2023-07-19 20:09:04 -0400

    Update EvaluationCurveDrawer.cs

--- Editor/EvaluationCurveDrawer.cs
+++ Editor/EvaluationCurveDrawer.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Reflection.Emit;
-using NUnit.Framework;
 using UnityEditor;
-using UnityEditor.Graphs;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Codesign
@@ -14,130 +10,146 @@ namespace Codesign
     public class EvaluationCurveDrawer : PropertyDrawer
     {
         private bool show;
+        private float testValue;
 
-        [UnityEngine.Range(2, 250)] private int curveResolution = 50;
+        private float width = 100;
+        private float height = 200;
+        [SerializeField ]private int margin = 25;
+
+        [UnityEngine.Range(2, 250)] private int curveResolution = 5;
+        private SerializedProperty ExponetialGain;
+        private SerializedProperty LinearGain;
+        private SerializedProperty Floor;
+
+        private float max;
+        private float min;
+        private float range;
+
+        private GUIStyle containerStyle;
+        private GUIStyle gridLabelStyle;
+        private GUIStyle PointLabelStyle;
 
         public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
         {
-            show = EditorGUI.Foldout(rect, show, label);
-            if (show)
-            {
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("Floor"), new GUIContent("Floor"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("LinearGain"), new GUIContent("Linear Gain"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ExponetialGain"), new GUIContent("Exponetial Gain"));
-                EditorGUI.indentLevel--;
-            }
-        }
+            ExponetialGain = property.FindPropertyRelative("ExponetialGain");
+            LinearGain = property.FindPropertyRelative("LinearGain");
+            Floor = property.FindPropertyRelative("Floor");
+            Styles();
 
-        public Rect GetNextPosition(Rect rect, int position) {
-            Rect nextPosition = rect;
-            nextPosition.y += EditorGUIUtility.singleLineHeight * position;
-            return nextPosition;
-        }
-
-        private float ExponetialGain;
-        private float LinearGain;
-        private float Floor;
-
-
-        private void v1Drawer(SerializedProperty property)
-        {
-            ExponetialGain = property.FindPropertyRelative("ExponetialGain").floatValue;
-            LinearGain = property.FindPropertyRelative("LinearGain").floatValue;
-            Floor = property.FindPropertyRelative("Floor").floatValue;
+            
+            EditorGUI.indentLevel ++;
+            //GUI.backgroundColor = new Color(2f, 2f, 2f);
+            
+            EditorGUILayout.BeginVertical(containerStyle);
 
             EditorGUILayout.BeginHorizontal();
-
-            var rectWidth = EditorGUIUtility.currentViewWidth * 0.5f;
-            var rectHeight = 200;
-
-            Rect layoutRectangle = GUILayoutUtility.GetRect(rectWidth, rectHeight);
-            EditorGUI.DrawRect(layoutRectangle, new Color(0.2f, 0.2f, 0.2f));
-            GUI.BeginClip(layoutRectangle);
-
-            GridCalc(rectWidth, rectHeight);
-
-            var extent = Evaluate(curveResolution);
-
-            Handles.color = Color.red;
-            Handles.DrawAAPolyLine(
-                Texture2D.whiteTexture,
-                2,
-                CurveCalc(rectWidth, rectHeight)
-                );
-            GUI.EndClip();
-
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("ExponetialGain"), new GUIContent("Exponetial Gain"));
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("LinearGain"), new GUIContent("Linear Gain"));
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("Floor"), new GUIContent("Floor"));
-            curveResolution = EditorGUILayout.IntField(new GUIContent("Evaluate Up To"), curveResolution);
-            EditorGUILayout.EndVertical();
+                show = EditorGUILayout.Foldout(show, label, true);
+                GUILayout.FlexibleSpace();
+                
+                GUILayout.Label($"[1]{Evaluate(1)} | [{curveResolution}]{Evaluate(curveResolution)}");
             EditorGUILayout.EndHorizontal();
-            EditorGUILayout.Space(30);
+
+            if (property != null && show) {
+                EditorGUILayout.Space(5);
+                width = EditorGUIUtility.currentViewWidth - 50;
+                GraphVisual();
+
+                max = MathF.Max(Evaluate(curveResolution), Evaluate(0));
+                min = MathF.Min(Evaluate(curveResolution), Evaluate(0));
+                range = MathF.Abs(Evaluate(curveResolution) - Evaluate(0));
+
+                EditorGUILayout.Space(5);
+                EditorGUILayout.PropertyField(Floor, new GUIContent("Floor"));
+                EditorGUILayout.PropertyField(LinearGain, new GUIContent("Linear Gain"));
+                EditorGUILayout.PropertyField(ExponetialGain, new GUIContent("Exponetial Gain"));
+                curveResolution = EditorGUILayout.IntField(new GUIContent("Evaluate Up To"), curveResolution);
+                EditorGUILayout.Space(5);
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUI.indentLevel--;
         }
 
-        private Vector3[] CurveCalc(float width, float height)
+        private void GraphVisual() {
+            Rect layoutRectangle = GUILayoutUtility.GetRect(200, height);
+            EditorGUI.DrawRect(layoutRectangle, new Color(0.18f, 0.18f, 0.18f));
+            //GUI.Box(layoutRectangle);
+            GUI.BeginClip(layoutRectangle);
+            GridCalc();
+            CurveCalc();
+            GUI.EndClip();
+        }
+
+        private void GridCalc()
         {
+            var gridCount = 5f;
+            var gridVert = (height - (margin * 2)) / (gridCount);
+
+            for (int i = 0; i <= gridCount; i++)
+            {
+                Handles.color = new Color(6, 6, 6);
+
+                float verticalLocation = (height - margin) - (gridVert * i);
+                float verticalValue = (range * (i/ gridCount)) + min;
+                //verticalValue = Evaluate(i);
+
+                string gridLabel = (max < 20 ? verticalValue.ToString("0.#") : MathF.Round(verticalValue).ToString());
+
+                Handles.Label(new Vector2(10 + (margin / 2), verticalLocation - (gridLabelStyle.CalcSize(new GUIContent(gridLabel)).y/2)), gridLabel, gridLabelStyle);
+
+                Handles.DrawAAPolyLine(
+                   Texture2D.grayTexture,
+                   0.3f,
+                   new Vector3(20 + (margin / 2), verticalLocation, 0),
+                   new Vector3(width  - (margin / 2), verticalLocation, 0)
+                );
+            }
+        }
+
+        private Vector3[] CurveCalc()
+        {
+            var widthMultiplier = (width - (margin * 2) - 30) / (curveResolution);
+            var heightMultiplier = (height - (margin*2)) / range;
             var curvePoints = new List<Vector3>();
-            var widthMultiplier = width / curveResolution;
-            var extent = Evaluate(curveResolution);
-            var heightMultiplier = height / extent;
-            for (int i = 0; i < curveResolution; i++)
+
+            for (int i = 0; i <= curveResolution; i++)
             {
                 var j = Evaluate(i);
-                curvePoints.Add(new Vector3(i * widthMultiplier, height - j * heightMultiplier, 0));
+                curvePoints.Add(new Vector3(i * widthMultiplier + (margin) + 30, height - margin - (j * heightMultiplier) + (min * heightMultiplier), 0));
             }
 
-            GUI.Label(new Rect(curvePoints[1].x + 10, curvePoints[1].y - 5, 50, 15), new Vector2Int(1, Mathf.RoundToInt(Evaluate(1))).ToString());
+            Handles.color = Color.red;
+            Handles.DrawAAPolyLine(Texture2D.whiteTexture, 1, curvePoints.ToArray());
 
-            Handles.DrawSolidDisc(curvePoints[1], Vector3.back, 5);
+            
 
-            Handles.DrawSolidDisc(curvePoints.LastOrDefault(), Vector3.back, 5);
-            Handles.Label(curvePoints.LastOrDefault() - new Vector3(50, -5, 0), new Vector2Int(curveResolution, Mathf.RoundToInt(extent)).ToString());
+            for (int i = 0; i < curvePoints.Count; i++)
+            {
+                Handles.DrawSolidDisc(curvePoints[i], Vector3.back, 3);
+                Vector2 point = new Vector2(i, Evaluate(i));
+                string label = point.x.ToString() + "/" + point.y.ToString("0.#");
+                Handles.Label(new Vector2(curvePoints[i].x, curvePoints[i].y + 5), label, PointLabelStyle);
+            }
+
             return curvePoints.ToArray();
         }
 
-        private void GridCalc(float width, float height)
+        public void Styles()
         {
-            var extent = Evaluate(curveResolution);
-            var heightMultiplier = height / extent;
-            if (extent < 300)
-            {
-                var gridVert = 10 * heightMultiplier;
-                for (int i = 0; i < 300 / gridVert; i++)
-                {
-                    Handles.color = new Color(50, 50, 50);
-                    Handles.Label(new Vector3(5, height - gridVert * i), (i * 10).ToString());
-                    Handles.DrawAAPolyLine(
-                       Texture2D.whiteTexture,
-                       0.3f,
-                       new Vector3(30, height - gridVert * i, 0),
-                       new Vector3(width, height - gridVert * i, 0)
-                       );
-                }
-            }
-            if (100 < extent && extent < 3000)
-            {
-                var gridVert = 100 * heightMultiplier;
-                for (int i = 0; i < 3000 / gridVert; i++)
-                {
-                    Handles.DrawAAPolyLine(
-                       Texture2D.whiteTexture,
-                       1,
-                       new Vector3(0, height - gridVert * i, 0),
-                       new Vector3(width, height - gridVert * i, 0)
-                       );
-                }
-            }
+            containerStyle = new GUIStyle(EditorStyles.helpBox);
+            containerStyle.padding = new RectOffset(8, 8, 5, 5);
 
+            gridLabelStyle = GUI.skin.label;
+            gridLabelStyle.fontSize = 10;
+            gridLabelStyle.alignment = TextAnchor.MiddleRight;
 
+            PointLabelStyle = GUI.skin.label;
+            PointLabelStyle.fontSize = 10;
+            PointLabelStyle.alignment = TextAnchor.UpperCenter;
         }
 
         public float Evaluate(float x)
         {
-            var y = Mathf.Pow(Mathf.Abs(x * LinearGain - 1), ExponetialGain) + Floor;
+            var y = Mathf.Pow(x * LinearGain.floatValue, ExponetialGain.floatValue) + Floor.floatValue;
             return y;
         }
     }
